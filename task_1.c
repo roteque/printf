1d84bc70b24bc93d191894e9a1d3a6dd5c154ff5
Wahab <wabsid01@gmail.com>
2023-08-22 15:57:41 -0700

    task 0

--- task_1.c
+++ task_1.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int _print_id(const char *format, ...)
+int _printf(const char *format, ...)
 {	
 	int char_count = 0;
 	va_list list;
@@ -32,22 +32,22 @@ int _print_id(const char *format, ...)
 	int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
 	write(1, num_str, num_len);
 	char_count += num_len;
-        }
-        else if (*format == 'i')
-        {       int num = va_arg(list, int);
-        char num_str[25];
-        int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
-        write(1, num_str, num_len);
-        char_count += num_len;
-        }
-        else
-        {
-        write(1, format - 1, 2);
-        char_count += 2;
-        }
-        }
-        format++;
-        }
-        va_end(list);
-        return (char_count);
+	}
+	else if (*format == 'i')
+	{       int num = va_arg(list, int);
+	char num_str[25];
+	int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
+	write(1, num_str, num_len);
+	char_count += num_len;
+	}
+	else
+	{
+	write(1, format - 1, 2);
+	char_count += 2;
+	}
+	}
+	format++;
+	}
+	va_end(list);
+	return (char_count);
 }
