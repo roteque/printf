a64ce03cd7a98518acd32c1a987bb60147fadfed
Wahab <wabsid01@gmail.com>
2023-08-22 14:06:34 -0700

    save

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<limits.h>
+
 int _printf(const char *format, ...);
 
 
--- print_out2.c
+++ print_out2.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+int _print_id(const char *format, ...)
 {	int char_count = 0;
 	va_list list;
     
-
 	if (format == NULL)
 	return (-1);	
 	va_start(list, format);
@@ -12,27 +11,23 @@ int _printf(const char *format, ...)
 	while (*format)
 	{
 	if (*format != '%')
-	{
-	write(1, format, 1);
+	{	write(1, format, 1);
 	char_count++;
 	}
 	else
 	{
 	format++;
-
 	if (*format == '\0')
 	break;
 	else if (*format == 'd')
-	{
-	int num = va_arg(list, int);
+	{	int num = va_arg(list, int);
 	char num_str[25];
 	int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
 	write(1, num_str, num_len);
 	char_count += num_len;
 	}
 	else if (*format == 'i')
-	{
-	int num = va_arg(list, int);
+	{	int num = va_arg(list, int);
 	char num_str[25];
 	int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
 	write(1, num_str, num_len);
@@ -46,7 +41,6 @@ int _printf(const char *format, ...)
 	}
 	format++;
 	}
-
 	va_end(list);
 	return (char_count);
 }
