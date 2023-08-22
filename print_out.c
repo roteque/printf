bef4a76a7b74b1d647003d77678875b1b58082b3
Wahab <wabsid01@gmail.com>
2023-08-22 06:39:15 -0700

    ty

--- print_out.c
+++ print_out.c
@@ -11,46 +11,43 @@
  **/
 
 int _printf(const char *format, ...)
-{	int char_count = 0;
+{
+	int char_count = 0;
 	va_list list;
 
 	if (format == NULL)
 	return (-1);
 	va_start(list, format);
 	while (*format)
-	{
-	if (*format != '%')
-	{
-	write(1, format, 1);
-	char_count++;
-	}
-	else
+	{if (*format != '%')
+	{write(1, format, 1);
+		char_count++;
+	}else
 	{	format++;
 	if (*format == '\0')
 	break;
 	if (*format == '%')
-	{
-	write(1, format, 1);
+	{	write(1, format, 1);
 	char_count++;
-	}
-	else if (*format == 'c')
+	}else
+	{format++;
+	if (*format == '\0')
+	break;
+	if (*format == 'c')
 	{	char letter = va_arg(list, int);
 	write(1, &letter, 1);
 	char_count++;
-	}
-	else if (*format == 's')
-	{char *str = va_arg(list, char*);
+	}else if (*format == 's')
+	{	char *str = va_arg(list, char*);
 	int str_count = 0;
 
 	while (str[str_count] != '\0')
 	{str_count++;
-	}	write(1, str, str_count);
-	char_count = char_count + str_count;
+	}write(1, str, str_count);
+	char_count += str_count;
 	}
+	}	format++;
+        }
+    }	va_end(list);
+	return char_count;
 }
-	format++;
-}
-	va_end(list);
-	return (char_count);
-}
-
