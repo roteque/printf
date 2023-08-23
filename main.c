6a7bd6f78befa1cf542b873d1be3cab37eb28c06
Oluwasegun_Omogoye <olusomogoye@gmail.com>
2023-08-23 01:14:33 +0000

    task 1

--- main.c
+++ main.c
@@ -8,9 +8,12 @@
  * Return: Always 0
  */
 int _printf(const char *format, ...);
+int _printf_id(const char *format, ...);
 
 int main(void)
 {
+	int len = 5, len2 = 7;
+	
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
 	printf("Character:[%c]\n", 'H');
@@ -19,5 +22,10 @@ int main(void)
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Percent:[%%]\n");
 	printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+        printf("Len:[%d]\n", len2);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+
 	return (0);
 }
